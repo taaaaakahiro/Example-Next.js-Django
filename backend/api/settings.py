ac8bfb659a9ce245b2d5f6734882f031f292c0f9
takahiro <takapi854@gmail.com>
2022-08-03 00:48:21 +0900

    setup

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -26,7 +26,7 @@ env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-d1=1_nb20gdby@+)4_@4^aoir&(k#0u@t2mw@n3*bwocy!(+r0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
